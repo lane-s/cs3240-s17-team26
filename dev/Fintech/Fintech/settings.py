74bbdfc80810cb89597f27b0e0e0062ebd4c612d
sdc3bb <sdc3bb@virginia.edu>
2017-04-03 17:32:55 -0400

    fixed issue?

--- dev/Fintech/Fintech/settings.py
+++ dev/Fintech/Fintech/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'Fintech.views.logged_in'
+                'Fintech.views.logged_in',
                 'Fintech.views.company_user'
             ],
         },
